6ef2eeb71165343343923d9e9f8a0cd2751a298e
MarceloLeonCaceres <marcelo.leon.c@gmail.com>
2023-12-26 21:30:42 -0500

    294 How NOT to send HTTP Requests

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,11 +1,18 @@
-import { useState } from 'react';
+import { useState } from "react";
 
-import Places from './Places.jsx';
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
 
-  
+  fetch("http://localhost:3000/places")
+    .then(() => {
+      return Response.json();
+    })
+    .then((resData) => {
+      setAvailablePlaces(resData.places);
+    });
+
   return (
     <Places
       title="Available Places"
